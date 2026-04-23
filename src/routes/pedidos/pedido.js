373cabef511887f4accf19e5fdd7c164bfbf9367
Edson Silva <112991456+edson-fiusa@users.noreply.github.com>
2026-04-22 23:42:40 -0300

    Remove manual ID comments from pedidos model
    Removed manual user_id and product_id comments from the pedidos model.

--- src/routes/pedidos/pedido.js
+++ src/routes/pedidos/pedido.js
@@ -2,8 +2,6 @@ const {DataTypes} = require('sequelize')
 const db = require('../../conexao/conexao')
 
 const pedidos = db.define('pedidos', {
-    // Remova o usuario_id e produto_id manuais daqui. 
-    // As associações no server.js vão criar as chaves corretas.
     quantidade: { type: DataTypes.INTEGER, allowNull: false }
 })
 
