ecd407f22b2ebf6b22c0fbf7260f26977937e157
Edson Silva <112991456+edson-fiusa@users.noreply.github.com>
2026-04-23 21:28:48 -0300

    Establish model associations for Usuario and Pedido
    Define associations between Usuario, Pedido, and Produto models.

--- src/models/associations.js
+++ src/models/associations.js
@@ -2,11 +2,9 @@ const usuario = require('./usuarios/usuario');
 const Pedido = require('./pedidos/itemPedido');
 const Produto = require('./produtos/produtos');
 
-// Usuario tem muitos Pedidos (1:N)
 usuario.hasMany(Pedido, { foreignKey: 'usuarioId' });
 Pedido.belongsTo(usuario, { foreignKey: 'usuarioId' });
 
-// Pedido pertence a muitos Produtos (N:N)
 Pedido.belongsToMany(Produto, {
     through: 'itens_pedido',
     foreignKey: 'pedidoId',
