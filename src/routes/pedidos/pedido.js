d1f2916ad888dd9512d4dfec4738ef18e5161a24
edson <antoedson@gmail.com>
2026-04-24 17:07:16 -0300

    atualizacao de busca por id

--- src/routes/pedidos/pedido.js
+++
@@ -1,8 +0,0 @@
-const {DataTypes} = require('sequelize')
-const db = require('../../conexao/conexao')
-
-const pedidos = db.define('pedidos', {
-    quantidade: { type: DataTypes.INTEGER, allowNull: false }
-})
-
-module.exports = pedidos
